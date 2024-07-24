1dd9f63e4ebb1ce0a24173fa62ae3df2d0a407db
concienciadesventurada <concienciadesventurada@proton.me>
2024-07-24 13:46:30 -0600

    ugly ass bullet point; couldn't bother creating a proper snippet

--- lua/maps.lua
+++ lua/maps.lua
@@ -30,6 +30,21 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
+vim.keymap.set("n", "<leader>sb", function()
+    local lines = {
+        "#### ",
+        "",
+        "",
+        "",
+        "```bash",
+        "",
+        "```"
+    }
+    local current_line = vim.fn.line('.')
+    vim.fn.append(current_line, lines)
+    vim.cmd((current_line + 1) .. "normal! $")
+end, { noremap = true, silent = true })
+
 -- Makes executable
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
